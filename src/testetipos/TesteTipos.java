6c5ff94012503516135bffc6987a1687b012c7b4
ellycruzz <serena_ellen@hotmail.com>
2017-08-18 23:34:44 -0300

    TesteTipos

---
+++ src/testetipos/TesteTipos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testetipos;
+
+/**
+ *
+ * @author EllyCruzz
+ */
+public class TesteTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int idade = 30;
+      String valor = Integer.toString(idade);
+      System.out.println(valor);
+     
+     String valor1 = "30";
+     int idade1 = Integer.parseInt(valor1);
+        System.out.println(idade1);
+        
+        
+    }
+    
+}
